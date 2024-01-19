b2a40c9945f820a12d1cda2c2e5beef37047a384
Li-YunJing <145111681+Li-YunJing@users.noreply.github.com>
2024-01-19 14:52:57 +0800

    Add files via upload

--- stock_info.py
+++ stock_info.py
@@ -21,4 +21,4 @@ def stock_id(message):
         mes = "股票代號 :{} \n股票名稱 : {} \n產業別 : {} \n市場 : {}\n成交價 : {} \n昨收 : {} \n漲跌價 : {} \n漲跌幅 : {} \n振幅 : {} \n開盤價 : {} \n最高價 : {} \n最低價 : {} \n資本額 : {} \n市值 : {}".format(soup_stock_name[0], soup_stock_name[1], soup_stock_info[1].text, soup_stock_info[2].text, soup_stock_data[0], soup_stock_data[1], soup_stock_data[2], soup_stock_data[3], soup_stock_data[4], soup_stock_data[5],soup_stock_data[6],soup_stock_data[7], soup_stock_info[4].text, soup_stock_info[5].text)
         return mes
     except:
-        return "請輸入正確的股票代號"
+        return "正確輸入方式為\n(ex.個股資訊 2330 / 個股資訊 台積電)"
